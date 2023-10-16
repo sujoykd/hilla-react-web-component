610a22c6cd11cd9198b632848f4d86fda0dfb85a
Vlad Rindevich <vladrin@vaadin.com>
2023-10-16 15:44:48 +0300

    chore: convert css to CSSStyleSheet and import it from two entrypoints

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,101 @@
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import type { UserConfigFn } from 'vite';
-import { overrideVaadinConfig } from './vite.generated';
+import { dirname, resolve, relative } from 'node:path';
+import type { OutputAsset, OutputChunk } from 'rollup';
+import type { UserConfigFn, Plugin } from 'vite';
+import { vaadinConfig } from './vite.generated.js';
 
-const customConfig: UserConfigFn = (env) => ({
-  // Here you can add custom Vite parameters
-  // https://vitejs.dev/config/
-  plugins: [
-    react({ include: '**/*.tsx', }),
-  ],
-  build: {
-    minify: false,
-    rollupOptions: {
-      input: {
-        appMain: resolve(__dirname, 'frontend/index.ts'),
-        webComponent: resolve(__dirname, 'frontend/web-component.tsx'),
-      },
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`
-      }
+const index = resolve(__dirname, 'frontend/index.html');
+const app = resolve(__dirname, 'frontend/index.ts');
+const webComponent = resolve(__dirname, 'frontend/web-component.tsx');
+const vaadin = resolve(__dirname, 'frontend/generated/vaadin.ts');
+
+function makeCSSCode(style: string): string {
+  return `var css = new CSSStyleSheet();
+css.replaceSync("${style.split('\n').map((line) => line.trim()).join('')}");
+export { css };
+`
+}
+
+function constructedStyleSheetPlugin(): Plugin {
+  return {
+    apply: 'build',
+    enforce: 'post',
+    name: 'constructed-style-sheet-plugin',
+    generateBundle(_, bundle) {
+      const keys = Object.keys(bundle);
+      const styleCssKey = keys.find(
+        (i) =>
+          bundle[i].type == 'asset' &&
+          bundle[i].fileName.endsWith('.css')
+      );
+
+      const sharedJsKey = keys.find((i) => bundle[i].fileName.includes('shared'));
+
+      const style = (bundle[styleCssKey] as OutputAsset).source as string;
+      delete bundle[styleCssKey];
+
+      (bundle[sharedJsKey] as OutputChunk).code = `${makeCSSCode(style)}${(bundle[sharedJsKey] as OutputChunk).code}`;
+
+      const appKey = keys.find((i) => bundle[i].fileName.includes('index') || !bundle[i].fileName.includes('html'));
+      const webComponentKey = keys.find((i) => bundle[i].fileName.includes('webComponent'));
+
+      const buildDir = dirname(appKey);
+      (bundle[appKey] as OutputChunk).code = `import {css} from './${relative(buildDir, sharedJsKey)}';\n${(bundle[appKey] as OutputChunk).code.replace(/__IMPORTED_STYLES__/ugm, 'css')}`;
+      (bundle[webComponentKey] as OutputChunk).code = `import {css} from './${relative(buildDir, sharedJsKey)}';\n${(bundle[webComponentKey] as OutputChunk).code.replace(/__IMPORTED_STYLES__/ugm, 'css')}`;
     },
-  },
-});
+    transformIndexHtml: {
+      enforce: 'post',
+      transform(html) {
+        return html.replace(/<link rel="stylesheet".*$/mgu, '');
+      }
+    }
+  }
+}
 
-export default overrideVaadinConfig(customConfig);
+const customConfig: UserConfigFn = async (env) => {
+  const def = await vaadinConfig(env);
+
+  return ({
+    ...def,
+    // Here you can add custom Vite parameters
+    // https://vitejs.dev/config/
+    plugins: [
+      ...def.plugins,
+      constructedStyleSheetPlugin(),
+      react({ include: '**/*.tsx', }),
+    ],
+    optimizeDeps: {},
+    build: {
+      ...def.build,
+      minify: false,
+      cssCodeSplit: false,
+      rollupOptions: {
+        ...def.build?.rollupOptions,
+        input: {
+          index,
+          vaadin,
+          webComponent,
+        },
+        output: {
+          manualChunks(id) {
+            if (app.includes(id) || index.includes(id)) {
+              return 'app-main';
+            }
+
+            if (webComponent.includes(id)) {
+              return 'web-component';
+            }
+
+            if (vaadin.includes(id)) {
+              return 'vaadin';
+            }
+
+            return 'shared';
+          }
+        }
+      },
+    },
+  });
+};
+
+export default customConfig;
